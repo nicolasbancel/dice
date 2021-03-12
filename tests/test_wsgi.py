3b2e13977ebb312d5a5969a7001b55884d4063e1
nicolasbancel <nicolas.bancel@gmail.com>
2021-03-12 16:10:10 +0100

    tests

---
+++ tests/test_wsgi.py
@@ -0,0 +1,16 @@
+from flask_testing import TestCase
+from wsgi import app
+# on import la variable app (l'application)
+
+# faut que fonction commence par test_ pour que nose comprenne
+
+class TestViews(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_one_roll(self):
+        roll = self.client.get('/').json['roll']
+        self.assertIsInstance(roll, int)
+        self.assertGreater(roll,0)
+        self.assertLess(roll,7)
