7d423b4a1bc3e65e2f8f1f54188bcad15faf3300
nicolasbancel <nicolas.bancel@gmail.com>
2021-03-16 19:30:58 +0100

    testing error

--- notes.sh
+++ notes.sh
@@ -4,3 +4,30 @@ pipenv run nosetests
 
 # Diff between files
 git diff Pipfile
+
+# Pull request
+
+# Possible de commenter par ligne de code
+# GitHub issues : Issues to reproduce the bug
+# Fixes bug No 2 : bug number (-41:48)
+
+
+# Commandes de base heroku
+
+# voir l'app:
+heroku open
+
+heroku logs --tail
+
+# Commandes de base heroku
+
+# App can be found here
+# https://calm-cove-18496.herokuapp.com/
+
+# Run the web server
+# Just execute in shell this script belows
+FLASK_ENV=development pipenv run flask run
+# Head to http://localhost:5000/
+
+####### USE ADDONS #######
+heroku addons:create sentry:f1
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,19 @@
 import random
+
+
+# Si sentry est défini, on importe le sdk et configure l'app
+
+import os
+sentry_dsn = os.getenv('SENTRY_DSN',None)
+if sentry_dsn:
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+        sentry_sdk.init(
+            sentry_dsn,
+            integrations=[FlaskIntegration()]
+        )
+
+
 from flask import Flask, jsonify
 app = Flask(__name__)
 
@@ -9,9 +24,14 @@ def home():
 
 
 print(app)
-print(dir(app))
-print(app.config)
+#print(dir(app))
+#print(app.config)
 
 #
 # {'ENV': 'production', 'DEBUG': False, 'TESTING': False, 'PROPAGATE_EXCEPTIONS': None, 'PRESERVE_CONTEXT_ON_EXCEPTION': None, 'SECRET_KEY': None, 'PERMANENT_SESSION_LIFETIME': datetime.timedelta(days=31), 'USE_X_SENDFILE': False, 'SERVER_NAME': None, 'APPLICATION_ROOT': '/', 'SESSION_COOKIE_NAME': 'session', 'SESSION_COOKIE_DOMAIN': None, 'SESSION_COOKIE_PATH': None, 'SESSION_COOKIE_HTTPONLY': True, 'SESSION_COOKIE_SECURE': False, 'SESSION_COOKIE_SAMESITE': None,  'SESSION_REFRESH_EACH_REQUEST': #True, 'MAX_CONTENT_LENGTH': None, 'SEND_FILE_MAX_AGE_DEFAULT': datetime.timedelta(seconds=43200), 'TRAP_BAD_REQUEST_ERRORS': None, 'TRAP_HTTP_EXCEPTIONS': False, 'EXPLAIN_TEMPLATE_LOADING': False, 'PREFERRED_URL_SCHEME': 'http', 'JSON_AS_ASCII': True, 'JSON_SORT_KEYS': True, 'JSONIFY_PRETTYPRINT_REGULAR': False, 'JSONIFY_MIMETYPE': 'application/json', 'TEMPLATES_AUTO_RELOAD': None, 'MAX_COOKIE_SIZE': 4093}
 #
+
+@app.route('/error')
+def error():
+    # 1/0 should not work
+    return jsonify({'roll':1 / 0})
